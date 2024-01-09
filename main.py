7aa576d1e2c3c119a89b0303104ca2ca86a32441
amodarivindu <amodarivindu1998@gmail.com>
2024-01-09 11:39:38 +0530

    Shellscript

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-# This is a sample Python script.
+#!/bin/bash
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+lines =$(ls - lh $1 | wc -l)
+
+if [ $  # -ne 1 ]
+then
+echo "this script requires exactly one directory path passed to it"
+echo "please try again"
+exit 1
+fi
+
+echo "you have $(($lines -1)) objects in the $1 file"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
